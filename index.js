771a92ae3bcfaa42e9ea9314e927bed87e5e4c27
LeanAgLotfi <106394110+LeanAgLotfi@users.noreply.github.com>
2022-12-22 20:57:42 -0300

    Limit agregado

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ const prod = new productManager(productos);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.listen(8080, () => console.log('Server is up and running on port 8080'))
-app.get('/products', async (req,res)=>{
-    console.log('Mostrar Productos');
-    res.send(prod);
+
+
+app.get('/products', async (req, res)=>{
+    console.log(req.query);
+    const {limit} = req.query;
+    if(limit){
+        res.send(productos.slice(0, +limit))
+    }else{
+        res.send({productos});
+    }
 })
+
+
 app.get('/products/:pid', async (req, res)=>{
     console.log(req.params);
     const prodId = req.params.pid;
@@ -19,11 +28,9 @@ app.get('/products/:pid', async (req, res)=>{
     }
     res.send({id});
 })
-app.get('/products', async (req, res)=>{
-    console.log(req.query);
-    let products = await prod.getProduct();
-    let limit = req.query.limit;
-        if (!limit) return res.send({ status: "success", payload: products });
-        let productLimit = productos.filter((product, indice) => indice < limit);
-        res.send({ status: "success", payload: productLimit })
+
+app.get('/', async (req,res)=>{
+    console.log('Mostrar Productos');
+    res.send(prod);
+    
 })
